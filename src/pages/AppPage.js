02afed8f6c095871c9707564df169f19f0085a73
Swarga-codes <sd3526@srmist.edu.in>
2022-03-12 18:17:18 +0530

    fixed hamburger

--- src/pages/AppPage.js
+++ src/pages/AppPage.js
@@ -3,9 +3,10 @@ import Enterapp from '../components/Enterapp'
 const AppPage = () => {
     return (  
         <>
+        <div className="AppPage">
         <Navbar/>
         <Enterapp/>
-        
+        </div>
         </>
     );
 }
